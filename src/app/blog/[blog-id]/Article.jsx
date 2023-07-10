01756ebce9a639520227b16b12ad8a06f0cb2108
wey1998 <yitwey1998@hotmail.com>
2023-07-10 19:29:57 +0100

    Add styling to article component

--- src/app/blog/[blog-id]/Article.jsx
+++ src/app/blog/[blog-id]/Article.jsx
@@ -12,7 +12,7 @@ export default function Article() {
     date: "June 20, 2023",
     time: "9:00A.M.",
     content:
-      "Artificial Intelligence (AI) has come a long way in recent years, and its potential for the future is truly boundless. From self-driving cars to voice-activated virtual assistants, AI is reshaping industries and revolutionizing our daily lives.<br /><br />One of the key areas where AI is making a significant impact is in healthcare. With the ability to analyze vast amounts of medical data and identify patterns, AI is assisting in diagnosing diseases and developing personalized treatment plans. AI-powered robots are even being used in surgeries, improving precision and reducing risks. This transformative technology has the potential to save lives and improve patient outcomes.<br /><br />In the field of transportation, AI is paving the way for autonomous vehicles. These self-driving cars have the potential to make our roads safer, reduce traffic congestion, and increase fuel efficiency. Companies like Tesla and Waymo are at the forefront of this technological revolution, investing heavily in AI research and development. While there are still challenges to overcome, such as regulatory frameworks and public acceptance, the future of transportation looks promising with AI at its core.<br /><br />AI is also transforming the way we interact with technology. Voice-activated virtual assistants like Siri, Alexa, and Google Assistant have become commonplace, providing us with information, entertainment, and control over smart devices. Natural language processing and machine learning algorithms enable these virtual assistants to understand and respond to human commands, making our lives more convenient and efficient.<br /><br />As AI continues to advance, ethical considerations are also coming to the forefront. Ensuring the responsible and ethical use of AI technologies is crucial to prevent misuse and protect privacy. It is essential to establish regulations and guidelines to govern the development and deployment of AI systems. This includes addressing concerns such as bias in algorithms, data privacy, and transparency.<br /><br />In conclusion, the future of artificial intelligence holds immense potential. From healthcare to transportation to everyday technology, AI is transforming industries and enhancing our lives. As researchers and engineers continue to push the boundaries of AI, we can expect even more groundbreaking advancements that will shape the future of our society. It is important for us to embrace AI while ensuring its responsible and ethical implementation, so that we can harness its benefits for the betterment of humanity.",
+      "Artificial Intelligence (AI) has rapidly advanced in recent years, reshaping industries and revolutionizing our daily lives. From self-driving cars to voice-activated virtual assistants, AI has demonstrated its transformative power across various domains. In healthcare, AI is making a significant impact by analyzing vast amounts of medical data and identifying patterns that human experts might miss. This allows for more accurate diagnoses and the development of personalized treatment plans. AI-powered robots are also being used in surgeries, assisting surgeons with their precision and reducing the risk of errors, leading to improved patient outcomes.<br /><br />Furthermore, AI has the potential to revolutionize the field of transportation. Autonomous vehicles, driven by AI algorithms, are poised to transform the way we travel. Self-driving cars can navigate through complex road conditions and respond to real-time traffic data, leading to safer and more efficient transportation. These vehicles have the potential to reduce the number of accidents caused by human error and optimize traffic flow, thereby easing congestion and reducing emissions. As we move towards a more sustainable future, AI-powered transportation solutions will play a crucial role.<br /><br />In addition to healthcare and transportation, AI is transforming the way we interact with technology. Voice-activated virtual assistants like Siri, Alexa, and Google Assistant have become an integral part of our lives, providing us with instant access to information, entertainment, and smart home control. These virtual assistants leverage natural language processing and machine learning algorithms to understand and respond to human commands, making our interactions with technology more intuitive and seamless. They are constantly learning and improving, becoming better at understanding context and providing personalized experiences.<br /><br />As AI technologies continue to advance, the potential for its applications is limitless. AI has the ability to revolutionize industries, solve complex problems, and improve our quality of life. However, it is important to address ethical considerations and ensure responsible development and deployment of AI systems. Ensuring transparency, accountability, and fairness will be crucial in harnessing the full potential of AI for the benefit of humanity.",
     image: "test-image",
   };
 
@@ -21,27 +21,29 @@ export default function Article() {
       <div className={styles.details}>
         <h5 className={styles.category}>{testArticle.category}</h5>
         <h1 className={styles.header}>{testArticle.title}</h1>
-        <div>
+        <div className={styles["article-details-container"]}>
           <span className={styles["article-details"]}>
-            {testArticle.author} / {testArticle.time} GMT+1 • {testArticle.date}
+            <span className={styles["author-name"]}>{testArticle.author} </span>
+            <span className={styles.symbol}>/</span> {testArticle.time} GMT+1 •{" "}
+            {testArticle.date}
           </span>
         </div>
-        <Image
-          src={`/images/article/${testArticle.image}.jpg`}
-          alt="Article Image"
-          width={100}
-          height={100}
-          className={styles["article-image"]}
-        />
-        <div className={styles.content}>
-          {testArticle.content.split("<br /><br />").map((paragraph, index) => {
-            return (
-              <p key={index} className={styles.paragraph}>
-                {paragraph}
-              </p>
-            );
-          })}
-        </div>
+      </div>
+      <Image
+        src={`/images/article/${testArticle.image}.jpg`}
+        alt="Article Image"
+        width={100}
+        height={100}
+        className={styles["article-image"]}
+      />
+      <div className={styles.content}>
+        {testArticle.content.split("<br /><br />").map((paragraph, index) => {
+          return (
+            <p key={index} className={styles.paragraph}>
+              {paragraph}
+            </p>
+          );
+        })}
       </div>
     </div>
   );
