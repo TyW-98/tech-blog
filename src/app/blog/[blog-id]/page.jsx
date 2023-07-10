e801b6da8eef8d46d3c544e51b924689828d0f38
wey1998 <yitwey1998@hotmail.com>
2023-07-11 00:06:36 +0100

    Add subscribe component

--- src/app/blog/[blog-id]/page.jsx
+++ src/app/blog/[blog-id]/page.jsx
@@ -1,3 +1,4 @@
+import Subscribe from "@/components/subscribe/Subscribe";
 import Article from "./Article";
 import OtherArticles from "./OtherArticles";
 import styles from "./page.module.css";
@@ -7,6 +8,7 @@ export default function BlogContent(props) {
     <main>
       <div className={styles.container}>
         <Article />
+        <Subscribe />
         <OtherArticles />
       </div>
     </main>
