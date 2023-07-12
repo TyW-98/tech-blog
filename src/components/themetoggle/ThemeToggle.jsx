3e61cc0fcb8d8858be774f7fa1bc2bfb27e79998
wey1998 <yitwey1998@hotmail.com>
2023-07-12 23:49:15 +0100

    Add ThemeToggler component to navbar

---
+++ src/components/themetoggle/ThemeToggle.jsx
@@ -0,0 +1,11 @@
+import styles from "./themetoggle.module.css";
+
+export default function ThemeToggle() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.moon}>🌙</div>
+      <div className={styles.sun}>😎</div>
+      <div className={styles.indicator}></div>
+    </div>
+  );
+}
