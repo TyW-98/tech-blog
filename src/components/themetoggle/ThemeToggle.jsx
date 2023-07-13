514ba165ed6df6efdb74507a12c963ddc3cb48cf
wey1998 <yitwey1998@hotmail.com>
2023-07-13 01:56:58 +0100

    Add context to control theme

--- src/components/themetoggle/ThemeToggle.jsx
+++ src/components/themetoggle/ThemeToggle.jsx
@@ -1,10 +1,24 @@
+"use client";
+
+import { useContext } from "react";
 import styles from "./themetoggle.module.css";
+import { ThemeContext } from "@/context/ThemeContext";
 
 export default function ThemeToggle() {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  function handleTheme() {
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  }
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={handleTheme}>
       <div className={styles.moon}>🌙</div>
-      <div className={styles.indicator}></div>
+      <div
+        className={`${styles.indicator} ${
+          theme === "dark" ? styles.dark : styles.light
+        }`}
+      ></div>
       <div className={styles.sun}>😎</div>
     </div>
   );
